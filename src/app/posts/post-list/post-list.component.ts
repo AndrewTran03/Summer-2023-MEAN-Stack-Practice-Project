71dd693bb5df463d6db73be9c30401a5dea18eea
AndrewTran03 <andrewtranva@gmail.com>
2023-06-01 11:35:31 -0400

    06/01/23: Added a Post-List Component for Angular Front-End

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+    // posts = [
+    //   {title: "First Post", content: "This is the content of the first post"},
+    //   {title: "Second Post", content: "This is the content of the second post"},
+    //   {title: "Third Post", content: "This is the content of the third post"}
+    // ];
+    
+    // @Input() posts: Post[] = [];
+    
+    posts: Post[] = [];
+    private postSub: Subscription;
+
+    constructor(public postService: PostService) {}
+
+    ngOnInit() {
+        this.posts = this.postService.getPosts();
+        this.postSub = this.postService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
+    }
+
+    ngOnDestroy() {
+        this.postSub.unsubscribe();
+    }
+}
